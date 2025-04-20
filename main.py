8270dc1f4b903bfaa0e006202f3ea0f3970a1c65
Martin_Moncek <martinmonc@gmail.com>
2025-04-20 22:01:56 +0200

    Adding a fully functioning data import ready for data preprocesing

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import os
+from random import randint
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder_path = 'D:\\Documents\\____school\\MLF_Project\\Train'
+# Load the train data
 
+folder_path = 'Train'
 # Get a list of all files in the folder
 files = os.listdir(folder_path)
 Train_data = [np.load(os.path.join(folder_path, file)) for file in files if file.endswith('.npy')]
-# combine all arrays into a single array
+#add data to single array
+Train_data = np.stack(Train_data, axis=0)
+# import the marking for data
+Train_marking = np.genfromtxt('label_train.csv', delimiter=',', skip_header=1)
+#add the marking to one colum array
+Train_marking = Train_marking[:, 1]
 
+# print the shape of the data
+print (Train_data.shape)
+print(Train_marking.shape)
+random_index = randint(0, 1491)
 plt.figure()
-plt.imshow(Train_data[55], cmap='gray', interpolation='nearest')
-plt.title('Test Data 0')
+plt.imshow(Train_data[random_index], cmap='gray', interpolation='nearest')
+plt.title('Train Data' + str(random_index))
+plt.show()
+
+
+# Load the test data
+folder_path = 'Test'
+# Get a list of all files in the folder
+files = os.listdir(folder_path)
+Test_data = [np.load(os.path.join(folder_path, file)) for file in files if file.endswith('.npy')]
+#add data to single array
+Test_data = np.stack(Test_data, axis=0)
+# import the marking for data
+Test_marking = np.genfromtxt('test_format.csv', delimiter=',', skip_header=1) 
+#add the marking to one colum array
+Test_marking = Test_marking[:, 1]  
+
+# print the shape of the data
+print (Test_data.shape)
+print(Test_marking.shape) 
+random_index = randint(0, 121) 
+plt.figure()
+plt.imshow(Test_data[random_index], cmap='gray', interpolation='nearest')
+plt.title('Test Data' + str(random_index))
 plt.show()
 
