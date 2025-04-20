2b7f729199c913120a0cebdc8be1d940f54100af
moncekM <demonmartin34@gmail.com>
2025-04-20 18:59:07 +0200

    adding a first test of importing data, but still need some love

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+folder_path = 'D:\\Documents\\____school\\MLF_Project\\Train'
+
+# Get a list of all files in the folder
+files = os.listdir(folder_path)
+Train_data = [np.load(os.path.join(folder_path, file)) for file in files if file.endswith('.npy')]
+# combine all arrays into a single array
+
+plt.figure()
+plt.imshow(Train_data[55], cmap='gray', interpolation='nearest')
+plt.title('Test Data 0')
+plt.show()
+
